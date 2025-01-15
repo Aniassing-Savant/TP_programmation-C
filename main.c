90a79e34219bf48e571f6e64771bc11c4fee8132
Aniassing-Savant <aniassingsavant@gmail.com>
2025-01-15 12:10:54 -0800

    première version du programme: creation des librairies et comprendre le fonctionnement

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "test_libry.c"
+int main( void ) {
+
+ int result = power( 2, 3 );
+ printf( "2³ == %d\n", result );
+ result = fact( 6 );
+ printf( "6! == %d\n", result );
+
+ return EXIT_SUCCESS;
+}
